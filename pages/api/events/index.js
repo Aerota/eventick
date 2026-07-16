acc7e3f2187ad2684a2bdd490e1811f31d22c8e7
Binuka Methvin <105734702+Aerota@users.noreply.github.com>
2026-07-16 10:13:03 +0530

    Update index.js

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,4 +1,3 @@
-@"
 import { supabase } from '../../../lib/supabaseClient'
 
 export default async function handler(req, res) {
@@ -12,4 +11,3 @@ export default async function handler(req, res) {
   }
   res.status(405).end()
 }
-"@ | Out-File -FilePath pages/api/events/index.js -Encoding utf8

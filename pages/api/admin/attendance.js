953ffb3a21b1d5408f2d19dc80dafdc3c0355979
Binuka Methvin <105734702+Aerota@users.noreply.github.com>
2026-07-16 10:14:49 +0530

    Update attendance.js

--- pages/api/admin/attendance.js
+++ pages/api/admin/attendance.js
@@ -1,4 +1,3 @@
-@"
 import { supabase } from '../../../lib/supabaseClient'
 
 export default async function handler(req, res) {
@@ -29,4 +28,3 @@ export default async function handler(req, res) {
   }
   res.status(405).end()
 }
-"@ | Out-File -FilePath pages/api/admin/attendance.js -Encoding utf8
